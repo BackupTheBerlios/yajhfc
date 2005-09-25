0250e40f823bd83be0d596bb5731ffba5f4b10b7
jonas <jonas>
2005-09-25 10:42:12 +0000

    Initial upload

---
+++ gnu-hylafax/gnu/hylafax/job/SendAndReceiveNotifier.java
@@ -0,0 +1,80 @@
+// SendAndReceiveNotifier.java - a HylaFAX Job representation
+// $Id: SendAndReceiveNotifier.java,v 1.1 2005/09/25 10:42:12 jonas Exp $
+//
+// Copyright 2003, Innovation Software Group, LLC - http://www.innovationsw.com
+//                Joe Phillips <joe.phillips@innovationsw.com>
+//
+// for information on the HylaFAX FAX server see
+//  http://www.hylafax.org/
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Library General Public
+// License as published by the Free Software Foundation; either
+// version 2 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Library General Public License for more details.
+//
+// You should have received a copy of the GNU Library General Public
+// License along with this library; if not, write to the Free
+// Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+package gnu.hylafax.job;
+
+// system includes
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+// home-grown includes
+import gnu.inet.ftp.ServerResponseException;
+
+/**
+ * This class is a convenience class implementing both a SendNotifier and ReceiveNotifier.
+ * @author $Author: jonas $
+ * @version $Id: SendAndReceiveNotifier.java,v 1.1 2005/09/25 10:42:12 jonas Exp $
+ * @see gnu.hylafax.job.SendListener
+ * @see gnu.hylafax.job.ReceiveListener
+ * @see gnu.hylafax.job.SendEvent
+ * @see gnu.hylafax.job.ReceiveEvent
+ **/
+public class SendAndReceiveNotifier 
+		extends BasicSendNotifier 
+		implements ReceiveNotifier
+{
+	private BasicReceiveNotifier rn;
+
+	public SendAndReceiveNotifier()
+	{
+		super();
+		rn= new BasicReceiveNotifier();
+	}
+
+	/**
+	 * This method is called when Job state changes.
+	 */
+	public void notifyReceiveListeners(ReceiveEvent details)
+	{
+		rn.notifyReceiveListeners(details); // delegate
+	}// notifyReceiveListeners
+
+	/**
+	 * This method is called to register a Job Listener.
+	 */
+	public void addReceiveListener(ReceiveListener l)
+	{
+		rn.addReceiveListener(l); // delegate
+	}
+
+	/**
+	 * This method is used to deregister a Job Listener.
+	 */
+	public void removeReceiveListener(ReceiveListener l)
+	{
+		rn.removeReceiveListener(l); // delegate
+	}
+
+}// SendAndReceiveNotifier class
+// SendAndReceiveNotifier.java
